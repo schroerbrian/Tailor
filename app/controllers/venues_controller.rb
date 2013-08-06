9c8beaa78c4d926145f078955066c2a26ba5c9e5
Ian Yang <ianyangyi@gmail.com>
2013-08-06 11:43:00 -0700

    search working

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,51 +1,56 @@
 class VenuesController < ApplicationController
-  # GET /venues
-  # GET /venues.json
+  include VenuesHelper
+
   def index
-    @venues = Venue.all
+    # @venues = Venue.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @venues }
-    end
-
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.json { render json: @venues }
+    # end
     # renders venue found from search
     # includes tags annd items where gender is the param
 
   end
 
-  def show
-    @venue = Venue.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @venue }
-    end
-
-  end
-
-  def update
-    @venue = Venue.find(params[:id])
-
-    respond_to do |format|
-      if @venue.update_attributes(params[:venue])
-        format.html { redirect_to @venue, notice: 'Venue was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @venue.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def search
+    @param = params
+    @title = params[:venue]
+    @gender = params[:gender]
 
-    # search from home page will take 2 params, one for gender and one for venue to
-    # return a json file with all the items
+    @weather = wunderweather
+
+    if @venue = Venue.find_by_name(@title)
+      render 'index'
+    else
+      redirect_to '/'
+    end
 
   end
-
-
-
 
 end
+
+  # def show
+  #   @venue = Venue.find(params[:id])
+
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.json { render json: @venue }
+  #   end
+  # end
+
+  # def update
+  #   @venue = Venue.find(params[:id])
+
+  #   respond_to do |format|
+  #     if @venue.update_attributes(params[:venue])
+  #       format.html { redirect_to @venue, notice: 'Venue was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @venue.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+
