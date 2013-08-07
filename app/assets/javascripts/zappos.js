71e9aba4ca049fad5c0e574410b66411599cf9be
Lior Bendat <liorbendat@gmail.com>
2013-08-06 21:31:30 -0700

    Working Zappos Api engine

--- app/assets/javascripts/zappos.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-      dataType: "jsonp",
-      url: 'http://api.zappos.com/Search?term=boots&filters={"productRating":"5"}&sort={"productPopularity":"desc"}&callback=wrapper&key=27b076f861701c2532e22f21b7b455c545afde4c',
-      data: data,
-      success: function(response) {
-      if (response['success'])
-          loadData(response['html']);
-      }
-})
