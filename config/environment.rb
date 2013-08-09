a481fe592cf31afcb448546a451d4aa752f9254b
Lior Bendat <liorbendat@gmail.com>
2013-08-08 18:29:48 -0700

    environment sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Taylor::Application.initialize!
+
+#configuration for SendGrid - Heroku
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.default_charset = "utf-8"
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = true
+config.action_mailer.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :domain => ENV['SENDGRID_DOMAIN'],
+  :user_name =>  ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :authentication => 'plain',
+  :enable_starttls_auto => true
+}
