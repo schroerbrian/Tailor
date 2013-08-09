34be2eb236572891e01e9be938d03a7108e1aff1
Lior Bendat <liorbendat@gmail.com>
2013-08-08 18:23:50 -0700

    sendgrid

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -1,15 +1,14 @@
-config.action_mailer.delivery_method = :sendmail
-
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_options = {from: 'no-reply@example.com'}
-
 config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.default_charset = "utf-8"
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = true
 config.action_mailer.smtp_settings = {
-  address:              'smtp.mail.yahoo.com',
-  port:                 465,
-  domain:               'example.com',
-  user_name:            '<tailorapp>',
-  password:             ENV['EMAIL_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :domain => ENV['SENDGRID_DOMAIN'],
+  :user_name =>  ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :authentication => 'plain',
+  :enable_starttls_auto => true
+}
