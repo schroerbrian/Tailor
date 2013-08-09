5d9cef25299ed8b0168d4e1ae07a624ba9a975f2
Brian Schroer <schroer.brian@gmail.com>
2013-08-08 20:41:48 -0700

    did css stuff. worked out ruby logic

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -6,10 +6,6 @@ class VenuesController < ApplicationController
 
   def search
 
-    venue_tag_ids = Venue.last.tags.map { |t| t.id }
-      @item_search = []
-      Item.all.each { |item| item_tag_ids = item.tags.map { |i| i.id }
-        @item_search << item if ((venue_tag_ids & item_tag_ids).count > 0) }
 
     @title = params[:venue]
     @gender = params[:gender]
@@ -17,15 +13,23 @@ class VenuesController < ApplicationController
     'Suits', 'Dresses', 'Jumpsuits and Rompers'].sample #need to add more, also with logic that contains gender
     @shoes_category = []
     @accessories_category = []
-
-    @weather = wunderweather
     @venue = Venue.find_by_name(@title)
+    @weather = wunderweather
+
+
+    venue_tag_ids = @venue.tags.map { |t| t.id }
+      @item_search = []
+      Item.all.each { |item| item_tag_ids = item.tags.map { |i| i.id }
+        @item_search << item if ((venue_tag_ids & item_tag_ids).count > 0) }
 
     if @venue 
+
       render 'index'
     else
       redirect_to '/'
     end
+
+
 
   end
 
