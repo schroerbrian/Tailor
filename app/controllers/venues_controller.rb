4af8a2a9412bb84499400ffc51e3af94570b5220
Ian Yang <ianyangyi@gmail.com>
2013-08-20 16:43:21 -0700

    stuff fixed

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -19,30 +19,30 @@ class VenuesController < ApplicationController
     # find venue
     @venue = Venue.find_by_name(@title)
 
-    # calling wunderweather
-    @weather = wunderweather
-
-    # find all the items associated with the venue
-    venue_tag_ids = @venue.tags.map { |t| t.id }
-
-    @item_search = []
-
-    Item.all.each { |item| item_tag_ids = item.tags.map { |i| i.id }
-    @item_search << item if ((venue_tag_ids & item_tag_ids).count > 0) }
-
-    @item_search.select! { |item|
-      item.gender == @gender
-    }
-
-    categories = []
-    @names = []
-    @item_search.each  {|item| categories << item.categories }
-    categories.each { |category| @names << category[0].name }
-    @names.uniq!
-
-    # load or not load
     if @venue
+      # calling wunderweather
+      @weather = wunderweather
+
+      # find all the items associated with the venue
+      venue_tag_ids = @venue.tags.map { |t| t.id }
+
+      @item_search = []
+
+      Item.all.each { |item| item_tag_ids = item.tags.map { |i| i.id }
+      @item_search << item if ((venue_tag_ids & item_tag_ids).count > 0) }
+
+      @item_search.select! { |item|
+        item.gender == @gender
+      }
+
+      categories = []
+      @names = []
+      @item_search.each  {|item| categories << item.categories }
+      categories.each { |category| @names << category[0].name }
+      @names.uniq!
+
       render 'index'
+
     else
       redirect_to '/'
     end
