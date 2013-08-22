4f7bd78f1fc6e6b538c44ea255f342aea521a5b9
Lior Bendat <liorbendat@gmail.com>
2013-08-21 18:39:52 -0700

    .js precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,11 @@ Taylor::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
 
 
   # Defaults to nil and saved in location specified by config.assets.prefix
